e6158fd19a727e5b1edd370ed75d2f6bbe571cf1
sivaneric <cimi.cimi@gmail.com>
2015-01-25 16:52:04 -0500

    cacheMatrix submission
    Solution Code for CacheMatrix Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,29 @@
 ## [Put comments here that describe what your functions do]
+#makeCacheMatrix creates a specail matrix vector, which is a list containing funtion to
+# 1. set the value of matrix 2. get the value of matrix, 3.set the inverse of matrix (solve) 
+# and 4. get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m <- x$getmatrix()
+  if (!is.null(m)) {
+    message("Getting Cached Matrix")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setmatrix(m)
+  m
+  ## Return a matrix that is the inverse of 'x'
 }
