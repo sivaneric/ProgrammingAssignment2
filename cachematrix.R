e5229f3e6793e50bbdd82b55a2f4a2cc994129e4
sivaneric <cimi.cimi@gmail.com>
2015-01-25 17:20:10 -0500

    cacheMatrix solution
    This is the submission for Programming Assignment 2; This solution has
    two functions, one that creates and caches a matrix vector and the
    second returns a inverse of the given matrix (it will return the cached
    value if the matrix has already been cached

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## [Put comments here that describe what your functions do]
-#makeCacheMatrix creates a specail matrix vector, which is a list containing funtion to
+# makeCacheMatrix creates a special matrix vector, which is a list containing funtion to
 # 1. set the value of matrix 2. get the value of matrix, 3.set the inverse of matrix (solve) 
 # and 4. get the inverse of the matrix
 
@@ -15,8 +14,10 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
-#cacheSolve returns a matrix that is inverse of the given matrix...if the matrix has already been 
-# cached,it returns the cahed value
+# cacheSolve returns a matrix that is inverse of the given matrix...if the matrix has already been 
+# cached,it returns the cached value
+#
+
 cacheSolve <- function(x=matrix(), ...) {
   m <- x$getmatrix()
   if (!is.null(m)) {
@@ -27,5 +28,5 @@ cacheSolve <- function(x=matrix(), ...) {
   m <- solve(matrix, ...)
   x$setmatrix(m)
   m
-  ## Return a matrix that is the inverse of 'x'
+  ## Returns a matrix that is the inverse of 'x'
 }
